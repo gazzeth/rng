8859b3e1480cc49dcbaf840bbb91e2b786903278
Alan Donoso Naumczuk <alan.donoso@mercadolibre.com>
2021-05-27 02:03:20 -0300

    Constant renamed

--- contracts/Rng.sol
+++ contracts/Rng.sol
@@ -5,13 +5,13 @@ import "./interfaces/IRng.sol";
 
 contract Rng is IRng {
 
-    uint256 constant MAX_BLOCKS = 256;
+    uint256 constant OLDEST_AVAILABLE_BLOCK = 256;
 
     function getRandomNumbers(uint256 _quantity) override external view returns (uint256[] memory) {
         uint256[] memory randomNumbers = new uint256[](_quantity);
         randomNumbers[0] = uint256(blockhash(block.number));
         for (uint256 i = 1; i < _quantity; i++) {
-            randomNumbers[i] = uint256(blockhash(block.number - randomNumbers[i - 1] % MAX_BLOCKS));
+            randomNumbers[i] = uint256(blockhash(block.number - randomNumbers[i - 1] % OLDEST_AVAILABLE_BLOCK));
         }
         return randomNumbers;
     }
