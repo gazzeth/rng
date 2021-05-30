54bc047c2952acf8bb4ab8b5f1c0e01fffcced81
Alan Donoso Naumczuk <alan.donoso@mercadolibre.com>
2021-05-29 23:56:38 -0300

    Ropsten depoy config fixed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 
-const INFURA_PROJECT_ID = "KEY";
+const INFURA_PROJECT_ID = "YOUR_INFURA_PROJECT_ID";
 
-const ROPSTEN_PRIVATE_KEY = "YOUR ROPSTEN PRIVATE KEY";
+const ROPSTEN_PRIVATE_KEY = "YOUR_ROPSTEN_PRIVATE_KEY";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -11,8 +11,8 @@ module.exports = {
   solidity: "0.7.6",
   networks: {
     ropsten: {
-      url: `https://ropsten.infura.io/v3/${INFURA_KEY}`,
-      accounts: [`0x${PRIVATE_KEY}`]
+      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
     }
   }
 };
