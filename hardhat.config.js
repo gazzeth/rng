7869f90fc8457d56b9c37e70faa3e21f50f214e5
Alan Donoso Naumczuk <alan.donoso@mercadolibre.com>
2021-05-29 23:55:34 -0300

    Deploy configuration added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
 
+const INFURA_PROJECT_ID = "KEY";
+
+const ROPSTEN_PRIVATE_KEY = "YOUR ROPSTEN PRIVATE KEY";
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.7.6",
+  networks: {
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${INFURA_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contract with the account:", deployer.address);
+    const Rng = await ethers.getContractFactory("Rng");
+    const rng = await Rng.deploy();
+    console.log("Contract address:", rng.address);
+}
+  
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
