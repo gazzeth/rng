9f0fc70567238ca7d98a9e13e40bdf7fdf03685d
Alan Donoso Naumczuk <alan.donoso@mercadolibre.com>
2021-05-31 01:52:11 -0300

    Deploy and verify config updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,21 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
-const INFURA_PROJECT_ID = "YOUR_INFURA_PROJECT_ID";
+// const INFURA_PROJECT_ID = "YOUR_INFURA_PROJECT_ID";
 
-const ROPSTEN_PRIVATE_KEY = "YOUR_ROPSTEN_PRIVATE_KEY";
+// const ROPSTEN_PRIVATE_KEY = "YOUR_ROPSTEN_PRIVATE_KEY";
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+// const ETHERSCAN_API_KEY = "YOUR_ETHERSCAN_API_KEY"
+
 module.exports = {
   solidity: "0.7.6",
   networks: {
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
-    }
+    // ropsten: {
+    //   url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
+    //   accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    // }
+  },
+  etherscan: {
+    // apiKey: `${ETHERSCAN_API_KEY}`
   }
 };
